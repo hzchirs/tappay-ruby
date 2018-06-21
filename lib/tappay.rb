db1496d4c7983b52cafc757fc01b7d0dab0b456d
HzChris <hzchirs@gmail.com>
2018-06-21 17:52:22 +0800

    Use class instance variable instead of class variable

--- lib/tappay.rb
+++ lib/tappay.rb
@@ -7,11 +7,11 @@ require 'tappay/pay_by_token'
 require 'tappay/refund'
 
 module TapPay
-  @@mode        = :sandbox
-  @@app_id      = ''
-  @@app_key     = ''
-  @@partner_key = ''
-  @@merchant_id = ''
+  @mode        = :sandbox
+  @app_id      = ''
+  @app_key     = ''
+  @partner_key = ''
+  @merchant_id = ''
 
   class << self
     def setup
@@ -19,43 +19,43 @@ module TapPay
     end
 
     def mode=(mode)
-      @@mode = mode
+      @mode = mode
     end
 
     def partner_key=(partner_key)
-      @@partner_key = partner_key.to_s
+      @partner_key = partner_key.to_s
     end
 
     def merchant_id=(merchant_id)
-      @@merchant_id = merchant_id.to_s
+      @merchant_id = merchant_id.to_s
     end
 
     def app_id=(app_id)
-      @@app_id = app_id
+      @app_id = app_id
     end
 
     def app_key=(app_key)
-      @@app_key = app_key
+      @app_key = app_key
     end
 
     def mode
-      @@mode
+      @mode
     end
 
     def partner_key
-      @@partner_key
+      @partner_key
     end
 
     def merchant_id
-      @@merchant_id
+      @merchant_id
     end
 
     def app_id
-      @@app_id
+      @app_id
     end
 
     def app_key
-      @@app_key
+      @app_key
     end
 
     def pay_by_prime(payload, &block)
