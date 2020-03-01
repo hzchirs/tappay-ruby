c73abfdf76a441c69dc77289c1e5f2355d770cdc
HzChris <hzchirs@gmail.com>
2020-03-01 16:44:15 +0800

    Use attr_accessor

--- lib/tappay.rb
+++ lib/tappay.rb
@@ -18,53 +18,10 @@ module TapPay
     def_delegators TapPay::Payment, :pay_by_prime, :pay_by_token
     def_delegators TapPay::Transaction, :refund
 
+    attr_accessor :mode, :partner_key
+
     def setup
       yield(self)
-    end
-
-    def mode=(mode)
-      @mode = mode
-    end
-
-    def partner_key=(partner_key)
-      @partner_key = partner_key.to_s
-    end
-
-    def merchant_id=(merchant_id)
-      warn <<-DEPRECATION_WARNING
-        [Deprecation] setting `merchant_id` in config options will be deprecated in the future releases.
-        It is recommaned to pass `merchant_id` to request params directly.
-      DEPRECATION_WARNING
-
-      @merchant_id = merchant_id.to_s
-    end
-
-    def app_id=(app_id)
-      warn '[Deprecation] setting `app_id` in config options will be deprecated in the future releases.'
-
-      @app_id = app_id
-    end
-
-    def app_key=(app_key)
-      warn '[Deprecation] setting `app_key` in config options will be deprecated in the future releases.'
-
-      @app_key = app_key
-    end
-
-    def mode
-      @mode
-    end
-
-    def partner_key
-      @partner_key
-    end
-
-    def app_id
-      @app_id
-    end
-
-    def app_key
-      @app_key
     end
   end
 end
